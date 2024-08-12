2233f99077f24764fd68adee04a6f57a5c04afcd
njp1cob <p.anjana@in.bosch.com>
2024-08-12 12:47:58 +0530

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,5 +1,5 @@
-const socbreach = require('./checkTemperatureWarning');
-const temperatureWarning = require('./checkSocBreach');
+const temperatureWarning = require('./checkTemperatureWarning');
+const socbreach = require('./checkSocBreach');
 const chargerateWarning = require('./checkchargeRateWarning');
 
 
@@ -13,8 +13,8 @@ function batteryIsOk(temperature,  soc,  chargeRate) {
     chargeRate = chargeRate
     let value = ((temperature < 0 || temperature > 45) || (soc < 20 || soc > 80) || (chargeRate > 0.8)) ? false: true;
     console.log(temperatureWarning(temperature)); 
-     // console.log(socbreach(soc));
-     // console.log(chargerateWarning(chargeRate));  
+     console.log(socbreach(soc));
+     console.log(chargerateWarning(chargeRate));  
     return value;
 
 }
