4036ecd79b086f419f7e74e7ad5759fa9d234686
njp1cob <p.anjana@in.bosch.com>
2024-08-12 12:27:37 +0530

    Update checkTemperatureWarning.js

--- checkTemperatureWarning.js
+++ checkTemperatureWarning.js
@@ -3,9 +3,9 @@ const { temperature } = require('./checker');
 
 function temperatureWarning(temperature) {
   let warning = 
-    temperature > 0 && value <= 2.5 ? "LOW_Temperature":
-    temperature > 2.5 && value <= 42.75 ? "Normal":
-    temperature > 42.75 && value <= 45 ? "High Temperature":
+    temperature > 0 && temperature <= 2.5 ? "LOW_Temperature":
+    temperature > 2.5 && temperature <= 42.75 ? "Normal":
+    temperature > 42.75 && temperature <= 45 ? "High Temperature":
     temperature > 45 ? "Temperature is out of range": "Invalid values";
   return warning;
 }
