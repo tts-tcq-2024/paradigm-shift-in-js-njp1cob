0277ce6ddff27a9fdbcf01907701439d41bbc297
njp1cob <p.anjana@in.bosch.com>
2024-08-12 12:25:29 +0530

    Update checker.js

--- checker.js
+++ checker.js
@@ -12,9 +12,9 @@ function batteryIsOk(temperature,  soc,  chargeRate) {
     soc = soc;
     chargeRate = chargeRate
     let value = ((temperature < 0 || temperature > 45) || (soc < 20 || soc > 80) || (chargeRate > 0.8)) ? false: true;
-    temperatureWarning(value);
-    socbreach(value);
-    chargerateWarning(value);  
+    temperatureWarning(temperature);
+    socbreach(soc);
+    chargerateWarning(chargeRate);  
     return value;
 
 }
