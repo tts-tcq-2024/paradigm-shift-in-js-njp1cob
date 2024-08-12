298158a5aaff4d307f0c9c590b390d4ee727c278
njp1cob <p.anjana@in.bosch.com>
2024-08-12 13:02:10 +0530

    Update checker.js

--- checker.js
+++ checker.js
@@ -28,8 +28,9 @@ function ExpectTrueOrFalse(expression) {
     }
 }
 function main() {
-    // ExpectTrueOrFalse(batteryIsOk(25, 70, 0.7));
+    
     ExpectTrueOrFalse(batteryIsOk(50, 85, 0.0));
+    ExpectTrueOrFalse(batteryIsOk(25, 70, 0.7));
    
 }
 
