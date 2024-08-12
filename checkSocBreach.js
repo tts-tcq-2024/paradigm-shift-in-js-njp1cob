725fe7bf3fa094b77f3cbabaa9a0ddfa07a25033
njp1cob <p.anjana@in.bosch.com>
2024-08-12 12:10:01 +0530

    Update checkSocBreach.js

--- checkSocBreach.js
+++ checkSocBreach.js
@@ -1,12 +1,12 @@
 
-const { temperature } = require('./checker');
+const { soc } = require('./checker');
 
-function socbreach(temperature) {
+function socbreach(soc) {
   let warning = 
-    temperature > 0 && value <= 20 ? "LOW_SOC_Breach":
-    temperature > 20 && value <= 24 ? "Approaching discharge":
-    temperature > 24 && value <= 76 ? "Normal":
-    temperature > 76 && value <= 80 ? "Approaching charge peak"
-    temperature > 80 ? "SOC is out of range!": "Invalid values";
+    soc > 0 && value <= 20 ? "LOW_SOC_Breach":
+    soc > 20 && value <= 24 ? "Approaching discharge":
+    soc > 24 && value <= 76 ? "Normal":
+    soc > 76 && value <= 80 ? "Approaching charge peak"
+    soc > 80 ? "SOC is out of range!": "Invalid values";
   return warning;
 }
