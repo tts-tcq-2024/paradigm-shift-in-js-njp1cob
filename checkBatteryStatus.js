b5326aa3a1a355e29c39d5539457436eea8afff4
njp1cob <p.anjana@in.bosch.com>
2024-08-12 10:11:17 +0530

    Update checkBatteryStatus.js

--- checkBatteryStatus.js
+++ checkBatteryStatus.js
@@ -1,1 +1,12 @@
 
+const { value } = require('./checker');
+
+function socbreach(value) {
+  let warning = 
+    value > 0 && value <= 20 ? "LOW_SOC_Breach":
+    value > 20 && value <= 24 ? "LOW_SOC_WARNING":
+    value > 24 && value <= 75 ? "Normal":
+    value > 75 && value <= 80 ? "HIGH_SOC_Warning":
+    value > 80 && value <==100 ? "HIGH_SOC_Breach";
+  return value;
+}
