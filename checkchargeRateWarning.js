b4499fa15ea348752de8afbcaaaaa4e2609af503
njp1cob <p.anjana@in.bosch.com>
2024-08-12 12:11:10 +0530

    Update checkchargeRateWarning.js

--- checkchargeRateWarning.js
+++ checkchargeRateWarning.js
@@ -1,9 +1,9 @@
-
-function chargerateWarning(value) {
+const { chargeRate } = require('./checker');
+function chargerateWarning(chargeRate) {
   let warning = 
-    value > 0 && value <= 0.04 ? "LOW_ChargeRate":
-    value > 0.04 && value <= 0.76 ? "Normal":
-    value > 0.76 && value <= 0.8 ? "Approaching High Charge Rate":
-    value > 0.8 ? "Charge Rate is out of range!": "Invalid values";
+    chargeRate > 0 && value <= 0.04 ? "LOW_ChargeRate":
+    chargeRate > 0.04 && value <= 0.76 ? "Normal":
+    chargeRate > 0.76 && value <= 0.8 ? "Approaching High Charge Rate":
+    chargeRate > 0.8 ? "Charge Rate is out of range!": "Invalid values";
   return warning;
 }
