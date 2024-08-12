2f2ae6eca98d8b345bfb7dd43c93f950f3aa6fee
njp1cob <p.anjana@in.bosch.com>
2024-08-12 12:14:25 +0530

    Update checkSocBreach.js

--- checkSocBreach.js
+++ checkSocBreach.js
@@ -6,7 +6,7 @@ function socbreach(soc) {
     soc > 0 && value <= 20 ? "LOW_SOC_Breach":
     soc > 20 && value <= 24 ? "Approaching discharge":
     soc > 24 && value <= 76 ? "Normal":
-    soc > 76 && value <= 80 ? "Approaching charge peak"
+    soc > 76 && value <= 80 ? "Approaching charge peak":
     soc > 80 ? "SOC is out of range!": "Invalid values";
   return warning;
 }
